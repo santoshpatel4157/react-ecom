893eaa121296064f8ba886b1838ad82de3757946
santoshpatel4157 <asmisthecat@gmail.com>
2023-04-05 18:59:59 +0530

    api add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Navbar } from "./comp/Navbar";
+import { Body } from "./comp/Body";
+import { Footer } from "./comp/Footer"
+
+
+
+
+const ApplayOut = () =>{
+  
+
+  return (
+    <>
+    
+    <Navbar />
+    <Body />
+    <Footer />
+    </>
+  )
+}
+
+
+
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <ApplayOut />
   );
 }
 
