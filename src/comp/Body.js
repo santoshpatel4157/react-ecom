ed3de3d4289c42a295aed02898e814285096e8a8
santoshpatel4157 <asmisthecat@gmail.com>
2023-04-06 16:03:07 +0530

    build

--- src/comp/Body.js
+++ src/comp/Body.js
@@ -1,13 +1,14 @@
 import { useState } from "react"
 import { Card } from "./Card";
 import { data } from "./data";
+import Shimmer from "./Shimmer";
 
 export const Body = () =>{
 
     const [searchTxt, setSearchTxt] =useState("");
     const [allMovies,setAllMovies] = useState(data);
     //  console.log(allMovies)
-    //const [filterMovie , setFilterMovie] = useState(data)
+    const [filterMovie , setFilterMovie] = useState(data)
     function filterData(searchTxt , allMovies){
         // console.log(allMovies)
         const filterData1 = allMovies.filter((movies)=>
@@ -16,14 +17,18 @@ export const Body = () =>{
        
     }
     
-    return (
+    if(allMovies === 0 ) return <h1>data not found</h1>
+
+    if(filterMovie === 0) return <h1>movie not found</h1>;
+
+    return (allMovies.length === 0 )? <Shimmer /> : (
         <>
         <input type="text" placeholder="search" value={searchTxt} onChange={(e)=>setSearchTxt(e.target.value)}/>
-    <button onClick={()=>{const data = filterData(searchTxt,allMovies); setAllMovies(data)}} >search</button>
+    <button onClick={()=>{const data = filterData(searchTxt,allMovies); setAllMovies(data) ; setFilterMovie(data)}} >search</button>
     {/* {console.log(allMovies)} */}
     <div className="card-list">
             {
-                allMovies.map((movies )=>{
+                filterMovie.map((movies )=>{
                     
                     return <Card {...movies}  key={movies.imdbID}/>
                 })
---
+++ src/comp/Shimmer.js
@@ -0,0 +1,5 @@
+const Shimmer = () => {
+    return (<h1>Shimmer lodding.....</h1>  );
+}
+ 
+export default Shimmer;
